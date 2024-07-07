40f69f38b2ef99f82ed61d3f470fbff7f425073e
Assaf1er <assaffart@gmail.com>
2024-07-07 13:01:32 +0100

    initial commit

---
+++ src/app/home.page.jsx
@@ -0,0 +1,19 @@
+import { Link } from "rasengan";
+import logo from "@assets/logo.svg";
+import Image from "@rasenganjs/image";
+
+const Home = () => {
+  return (
+    <section className="w-full h-full bg-white flex flex-col items-center py-8 px-[20px] md:px-[50px] xl:px-[200px] font-comfortaa">
+     
+    </section>
+  );
+};
+
+Home.path = "/";
+Home.metadata = {
+  title: "Home", 
+  description: "Home page",
+};
+
+export default Home;
